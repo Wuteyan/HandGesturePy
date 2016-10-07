d14a7be3076d7fcab24ead7ede62c10caba0b219
TeYanWu <killerw74@gmail.com>
2016-10-07 17:26:27 +0800

    test wait key

--- test.py
+++ test.py
@@ -19,4 +19,7 @@ time.sleep(0.1)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
     cv2.imshow("Image", image)
-    cv2.waitKey(0)
+    # cv2.waitKey(0)
+    k = 0xFF & cv2.waitKey(10)
+    if k == 27:
+        break
