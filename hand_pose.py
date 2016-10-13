0c543f71d4f3d59ab6f6d5668a173bd305f9ec86
mhci <r05922095@ntu.edu.tw>
2016-10-13 21:23:03 +0800

    add posPredict function in hand_pose.py

--- hand_pose.py
+++ hand_pose.py
@@ -30,7 +30,7 @@ class  Hand_Pose():
 		self.move_text={'1':'Rock','2':'Scissor','3':'Paper','4':'Scissor','5':'Paper'}
 		
 		self.buffer = []
-
+		self.resBuf = []
 		#The main event loop
 		# initialize the camera and grab a reference to the raw camera capture
 		self.camera = PiCamera()
@@ -42,7 +42,7 @@ class  Hand_Pose():
 		#create and train SVM model each time coz bug in opencv 3.1.0 svm.load() https://github.com/Itseez/opencv/issues/4969
 		rawCapture = PiRGBArray(self.camera, size=(640,480))
 		# rawCapture = PiRGBArray(camera)
-		 
+		self.resBuf[:] =[]
 		# allow the camera to warmup
 		time.sleep(0.1)
 
@@ -62,6 +62,7 @@ class  Hand_Pose():
 			cnt=ut.getMaxContour(contours,4000)
 			if cnt!=None:
 				gesture,res=ut.getGestureImg(cnt,img,th1,model)
+				self.resBuf.append(res)
 				print(res)
 				cv2.imshow('PredictedGesture',cv2.imread('SICTrainData/'+res+'_21.jpg'))
 				# move='         '+move_text[res]
@@ -77,6 +78,28 @@ class  Hand_Pose():
 
 		cap.release()        
 		cv2.destroyAllWindows()
+
+	def posPredict(self):
+		# check resBuf and get the most possible gesture
+		posRock = 0
+		posScissors = 0
+		posPapper = 0 
+		for pose in self.resBuf :
+			if pose == 1:
+				posRock = posRock + 1
+			elif pose == 2:
+				posScissors = posScissors + 1
+			elif pose == 3:
+				posPapper = posPapper + 1
+		posFinal = max(posPapper, posScissors, posRock)
+		# clear resBuf
+		self.resBuf[:] = []
+		if posFinal == posRock:
+			return 1
+		elif posFinal == posScissors:
+			return 2
+		else:  return 3
+
 
 	# def getbuffer(self):
 
