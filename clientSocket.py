5282f15fe2d6019fe54c40aeebee58cdd0e70f15
TeYanWu <killerw74@gmail.com>
2016-10-13 13:24:42 +0800

    update clientserver

--- clientSocket.py
+++ clientSocket.py
@@ -6,5 +6,6 @@ host = '10.0.1.21'# ip of raspberry pi
 port = 4001               
 s.connect((host, port))
 while 1:
-    print(s.recv(1024))
+    # print(s.recv(1024))
+    s.sendall("676987\n")
 s.close()
