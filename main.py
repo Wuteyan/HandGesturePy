3efd0b17f800be343f20dd26702d46cf899a7c31
acer\E1-571G <gp3wup6m3@gmail.com>
2016-10-14 08:50:11 +0800

    test

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     hand_pose = Hand_Pose()
     cc = ClientSocket.ClientSocket('10.0.1.33', 4001)
     cc.connect();
-    handPose.startCamera()
+    hand_pose.startCamera()
     #recvThread = Thread(target = cameraThreadFunc, args = (hand_pose,))
 
     try:
